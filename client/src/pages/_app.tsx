4fe0d774d218cd66439314e1eaca78c814e73442
wodmsgkgk <wodmsgkgk@naver.com>
2022-09-30 15:11:18 +0900

    login function added

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Axios from 'axios'
+import { AuthProvider } from '../routes/auth';
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
 
-  return <Component {...pageProps} />
+  return <AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>
 }
 
 export default MyApp
