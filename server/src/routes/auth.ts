131a186a4495941377981c4f20d146ee4478f663
wodmsgkgk <wodmsgkgk@naver.com>
2022-10-06 15:10:06 +0900

    logout function added

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -97,6 +97,21 @@ const login = async (req: Request, res: Response) => {
     }
 }
 
+const logout = async (_: Request, res: Response) => {
+  res.set(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: new Date(0),
+      path: "/",
+    })
+  );
+  res.status(200).json({ success: true });
+};
+
+
 const me = async (_: Request, res: Response) => {
     return res.json(res.locals.user)
 }
@@ -104,6 +119,7 @@ const me = async (_: Request, res: Response) => {
 const router = Router()
 router.post("/register", register)
 router.post("/login", login)
+router.post("/logout", userMiddleWare, authMiddleWare, logout)
 router.post("/me", userMiddleWare, authMiddleWare, me)
 
 export default router
