5c0c14b6749a1618a080d42b57d6002afbb0202d
wodmsgkgk <wodmsgkgk@naver.com>
2022-11-17 16:09:36 +0900

    커뮤니티 리스트 UI 생성

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: [
+      "www.gravatar.com",
+      "localhost",
+      "ec2-43-200-8-24.ap-northeast-2.compute.amazonaws.com"
+    ]
+  }
 }
 
 module.exports = nextConfig
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,14 +6,17 @@ import styles from '../styles/Home.module.css'
 import useSWR from 'swr'
 import { Sub } from '../types'
 import axios from 'axios'
+import { useAuthState } from '../context/auth'
 
 const Home: NextPage = () => {
+  const { authenticated } = useAuthState();
 
   const fetcher = async (url: string) => {
     return await axios.get(url).then(res => res.data)
   }
   const address = "http://localhost:4000/api/subs/sub/topSubs"
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher)
+  console.log('topSubs', topSubs)
 
   return (
     <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
@@ -27,16 +30,46 @@ const Home: NextPage = () => {
         <div className='bg-white border rounded'>
           <div className='p-4 border-b'>
             <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
+          </div>
+
+          {/* 커뮤니티 리스트 */}
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`}>
+                  <a>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                </Link>
+                <Link href={`/r/${sub.name}`}>
+                  <a className='ml-2 font-bold hover:cursor-pointer'>
+                    /r/{sub.name}
+                  </a>
+                </Link>
+                <p className='ml-auto font-md'>{sub.postCount}</p>
+              </div>
+            ))}
 
           </div>
-          <div></div>
-          <div className='w-full py-6 text-center'>
-            <Link href="/subs/create">
-              <a className='w-full p-2 text-center text-white bg-gray-400'>
-                커뮤니티 만들기
-              </a>
-            </Link>
-          </div>
+
+          {authenticated &&
+            <div className='w-full py-6 text-center'>
+              <Link href="/subs/create">
+                <a className='w-full p-2 text-center text-white bg-gray-400'>
+                  커뮤니티 만들기
+                </a>
+              </Link>
+            </div>
+          }
 
         </div>
       </div>
